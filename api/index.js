a39e6a886b5668542ae144cb707c612251772110
FarPelaez <farley.diseno@gmail.com>
2020-06-02 15:58:03 -0500

    creando la autenticacion

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const meals = require('./routes/meals');
 const orders = require('./routes/orders');
+const auth = require('./routes/auth');
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
@@ -12,5 +13,6 @@ mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopo
 
 app.use('/api/meals', meals);
 app.use('/api/orders', orders);
+app.use('/api/auth', auth);
 
 module.exports = app;
